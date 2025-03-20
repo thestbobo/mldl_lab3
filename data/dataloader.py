b3aa65d6713f8c6249274b9b5a35364cdd30b337
thestbobo <bobbiplus2002@icloud.com>
2025-03-20 17:14:37 +0100

    wandb visualization added on train.py

--- data/dataloader.py
+++ data/dataloader.py
@@ -13,6 +13,13 @@ class TinyImagenetDataLoader:
                                     T.Normalize(mean=[0.485, 0.456, 0.406],
                                                 std=[0.229, 0.224, 0.225])
                                     ])
+        self.train_transform = T.Compose([T.RandomResizedCrop(64),
+                                          T.RandomHorizontalFlip(),
+                                          T.RandomRotation(20),
+                                          T.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+                                          T.ToTensor(),
+                                          T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                        ])
         self.num_workers = num_workers
         self.batch_size = batch_size
         self.root_dir = "dataset/tiny-imagenet-200"
@@ -25,6 +32,6 @@ class TinyImagenetDataLoader:
 
     def get_train_loader(self):
         train_dataset = ImageFolder(root=os.path.join(self.root_dir, "train"),
-                                    transform=self.transform)
+                                    transform=self.train_transform)
         return DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
 
